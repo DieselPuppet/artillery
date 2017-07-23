232b0e0eedf97a629d2e69a5f30d051af8ce349e
Dmitry Sidorenko <daemon4d@gmai.com>
2017-07-23 21:13:35 +0300

    Round start handling

--- scenarios/my_functions.js
+++ scenarios/my_functions.js
@@ -1,6 +1,6 @@
 
 var AWS = require('aws-sdk');
-
+const debug=require('debug')('ws');
 var lambda = new AWS.Lambda({region:'eu-west-1'});
 
 exports.registerUser = function(baseURl, callback) {
@@ -11,5 +11,25 @@ exports.registerUser = function(baseURl, callback) {
           callback(baseURl+payload.identityId);           // successful response
        }
     });
-  return 'Hello world!';
+  return '';
+}
+
+function sendWord(ws, word) {
+    debug('Sending word:'+word);
+}
+
+function onRoundStart(ws,roundNo,roundData) {
+    debug("Starting round "+roundNo+":"+roundData.Word);
+    setTimeout(sendWord, 5000,ws, roundData.Word);
+}
+
+exports.receiveHandler = function(ws, textMsg) {
+    msg = JSON.parse(textMsg);
+    msgData = JSON.parse(msg.data);
+    if (msg.code == 'GameStateSync'){
+        debug("receive "+msg.code);
+        if (msgData.Rounds.length>msgData.RoundResults.length) {
+            onRoundStart(ws, msgData.Rounds.length, msgData.Rounds[msgData.Rounds.length-1]);
+        }
+    }
 }
